d6b004bb039a75ef54d069ca4e422eb50fff5bee
SvetlanaPrus <64580478+SvetlanaPrus@users.noreply.github.com>
2021-02-19 21:09:32 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ interface ITask {
     firstNumber: number
     sign: string
     secondNumber: number
-    sysAnswer: () => number      //otvet samoj sistemi na primer
+    sysAnswer: number      //otvet samoj sistemi na primer
     count: number
 }
 
 function App() {
     const [tasks, setTasks] = useState<ITask[]>([])                      //massiv primerov
     const [startIsPress, setStartIsPress] = useState(false)             //kontrol knopki 'Start'
-    const [count, setCount] = useState(0)                           //kontrol za kolichestvom primerov
+    const [count, setCount] = useState(1)                           //kontrol za kolichestvom primerov
     const [myAnswer, setMyAnswer] = useState('')                   //kontrol stroki 'input'
     // const [okIsPress, setOkIsPress] = useState(false)
     const [rightAnswer, setRightAnswer] = useState(0)
@@ -26,7 +26,7 @@ function App() {
 
 
     //peremennaja 'otvet sistemi na primer':
-    const sysAnswer = () => {
+    const getSysAnswer = () => {
         let sysResult = 0;
         switch (sign) {
             case "+":
@@ -44,25 +44,27 @@ function App() {
 
     //zapusk primera dlja polzovatelja:
     const quiz = () => {
-        setCount(count + 1);                                           //zapuskaem schetchik zadach
+        const newCount = count + 1;
+        setCount(newCount);                                           //zapuskaem schetchik zadach
         if (count === 10) {                                                  //esli uze 10 primerov - vihodim
             setCount(0)                                                 //schetchik obnuljaem
             exit()
         } else {
             setStartIsPress(true);                                     //menjaem status nazatija knopki 'Start'
-            const newQuiz = [...tasks, {firstNumber, sign, secondNumber, sysAnswer, count}]
+            const a = getSysAnswer();
+            const newQuiz = [...tasks, {firstNumber, sign, secondNumber, sysAnswer: a, count}]
             setTasks(newQuiz)
         }
     }
 
-    const save = (myAnswer: number, sysAnswer:number) => {
+    const save = (myAnswer: number) => {
         if (count === 10) {
             finish()
         } else {
-            myAnswer === sysAnswer ? setRightAnswer(rightAnswer + 1) : setWrongAnswer(wrongAnswer + 1)
-            console.log(rightAnswer, wrongAnswer)
+            myAnswer === rightAnswer ? setRightAnswer(rightAnswer + 1) : setWrongAnswer(wrongAnswer + 1)
         }
         setMyAnswer('');                                               //ochistka polja vvoda v 'input'
+        {quiz()}
     }
 
     const finish = () => {
@@ -76,6 +78,8 @@ function App() {
         setStartIsPress(false)
         setTasks([])
     }
+    console.log(tasks)
+    console.log(count)
 
     return (
         <div className="App">
@@ -85,9 +89,9 @@ function App() {
                     {tasks.map((el, index) =>
                         index === tasks.length - 1 ?
                             <div>
-                                {index + 1}) {el.firstNumber} {el.sign} {el.secondNumber} = <span hidden={true}>{el.sysAnswer()}</span>
+                                {index + 1}) {el.firstNumber} {el.sign} {el.secondNumber} = {el.sysAnswer}
                                 <input type='number' value={myAnswer} onChange={(e) => setMyAnswer(e.target.value)}/>
-                                <button onClick={ () => save(+myAnswer, sysAnswer()) }>Save</button>
+                                <button onClick={ () => save(+myAnswer)}>Save</button>
                             </div>
                             : <></>
                     )}
